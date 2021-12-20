cc85f67a34fe1803cce4bc7d06eec6b318a342a6
wenliang <lu@de42.net>
2021-12-20 12:12:23 -0800

    day 03 use recursive

--- 2021/day03.py
+++ 2021/day03.py
@@ -19,49 +19,38 @@ def count_most_least(group):
     return ("1", "0") if counts["1"] >= counts["0"] else ("0", "1")
 
 counts = [count_most_least(g) for g in groups]
-c_gamma, c_epsilon = list(zip(*counts))
+parameters = list(zip(*counts))
 
-b_gamma = "0b{}".format("".join(c_gamma))
-b_epsil = "0b{}".format("".join(c_epsilon))
-print("gamma in binary: {}, epsilon in binary: {}".format(b_gamma, b_epsil))
+b_para = ["0b{}".format("".join(c_para)) for c_para in parameters]
+print("gamma in binary: {}, epsilon in binary: {}".format(b_para[0], b_para[1]))
 
 # convert binary numbers to integer
-i_gamma = int(b_gamma, 2)
-i_epsil = int(b_epsil, 2)
-print("gamma: {}, epsilon: {}, power consumption: {}".format(i_gamma, i_epsil, i_gamma*i_epsil))
+i_para = [int(b, 2) for b in b_para]
+print("gamma: {}, epsilon: {}, power consumption: {}".format(i_para[0], i_para[1], i_para[0]*i_para[1]))
 
 #############################################################
 # life support rating
 
-def count_most_least_char(lines, index):
-    collect = [line[index] for line in lines]
-    counts = Counter(collect)
-    return ["1", "0"] if counts["1"] >= counts["0"] else ["0", "1"]
+# NOTE: cannot reuse the group above. because it is changing after each filter.
 
 def filter_lines(lines, most_or_least):
     assert most_or_least in [0, 1]
 
-    for i in range(n_char_per_line):
-        check_char = count_most_least_char(lines, i)[most_or_least]
-        left_lines = [line for line in lines if line[i] == check_char]
-        if len(left_lines) == 1:
-            return left_lines[0]
-        if len(left_lines) == 0:
-            print("nothing left. something wrong. break here")
-            1/0
-        lines = left_lines
+    if len(lines) == 1:
+        return lines[0]
+    if len(lines) == 0:
+        print("nothing left. something wrong. break here")
+        1/0
 
-    print("nothing left. something wrong. break here")
-    1/0
+    g1 = [line[0] for line in lines]
+    check_char = count_most_least(g1)[most_or_least]
+    left_lines = [line[1:] for line in lines if line[0] == check_char]
+    # NOTE: use recursive way
+    return [check_char] + filter_lines(left_lines, most_or_least)
 
+b_paras =  ["0b{}".format("".join(filter_lines(lines, i))) for i in range(2)]
+print("oxgen: {}, co2: {}".format(b_paras[0], b_paras[1]))
 
-b_oxygen_gen_rating = "0b{}".format("".join(filter_lines(lines, 0)))
-b_co2_scrubber_rating = "0b{}".format("".join(filter_lines(lines, 1)))
-
-print("oxgen: {}, co2: {}".format(b_oxygen_gen_rating, b_co2_scrubber_rating))
-
-i_oxygen = int(b_oxygen_gen_rating, 2)
-i_co2 = int(b_co2_scrubber_rating, 2)
-
-print("oxygen: {}, co2: {}, life support: {}".format(i_oxygen, i_co2, i_oxygen*i_co2))
+i_paras = [int(para, 2) for para in b_paras]
+print("oxygen: {}, co2: {}, life support: {}".format(i_paras[0], i_paras[1], i_paras[0]*i_paras[1]))
 
