4d02f85f5e52d560eb546cc5710d777ab8644d36
wenliang <lu@de42.net>
2021-12-20 11:30:55 -0800

    day03 rewritten in functional programming

--- 2021/day03.py
+++ 2021/day03.py
@@ -1,98 +1,62 @@
 #! /usr/bin/env python3
 
+from collections import Counter
+
 with open("day03_input.txt", "r") as f:
     lines_input = f.readlines()
 
 n_char_per_line = 12
 
+# 1000x12
+lines = [[char for char in line.strip()] for line in lines_input]
+# 12x1000
+groups = list(zip(*lines))
 
 #############################################################
 # first scan
-n_ones = [0] * n_char_per_line
-n_zeros = [0] * n_char_per_line
-for line in lines_input:
-    for i, char in enumerate(line):
-        if char == "1":
-            n_ones[i] += 1
-        elif char == "0":
-            n_zeros[i] += 1
-        else:
-            pass # this is \n
-            # print("{} not supported".format(char))
+def count_most_least(group):
+    counts = Counter(group)
+    return ("1", "0") if counts["1"] >= counts["0"] else ("0", "1")
 
-print("count of ones: {}".format(n_ones))
-print("count of zeros: {}".format(n_zeros))
+counts = [count_most_least(g) for g in groups]
+c_gamma, c_epsilon = list(zip(*counts))
 
-#  calculate gamma / epsilon
-b_gamma = "0b"
-b_epsil = "0b"
-
-for i in range(n_char_per_line):
-    if n_ones[i] > n_zeros[i]:
-        b_gamma += "1"
-        b_epsil += "0"
-    else:
-        b_gamma += "0"
-        b_epsil += "1"
-
+b_gamma = "0b{}".format("".join(c_gamma))
+b_epsil = "0b{}".format("".join(c_epsilon))
 print("gamma in binary: {}, epsilon in binary: {}".format(b_gamma, b_epsil))
 
 # convert binary numbers to integer
 i_gamma = int(b_gamma, 2)
 i_epsil = int(b_epsil, 2)
-
 print("gamma: {}, epsilon: {}, power consumption: {}".format(i_gamma, i_epsil, i_gamma*i_epsil))
 
 #############################################################
 # life support rating
 
-def count_most_common(lines, index):
-    n_1 = 0
-    n_0 = 0
-    for line in lines:
-        if line[index] == "1":
-            n_1 += 1
-        elif line[index] == "0":
-            n_0 += 1
-    if n_1 >= n_0:
-        return "1"
-    else:
-        return "0"
+def count_most_least_char(lines, index):
+    collect = [line[index] for line in lines]
+    counts = Counter(collect)
+    return ["1", "0"] if counts["1"] >= counts["0"] else ["0", "1"]
 
-def count_least_common(lines, index):
-    n_1 = 0
-    n_0 = 0
-    for line in lines:
-        if line[index] == "1":
-            n_1 += 1
-        elif line[index] == "0":
-            n_0 += 1
-    if n_1 >= n_0:
-        return "0"
-    else:
-        return "1"
+def filter_lines(lines, most_or_least):
+    assert most_or_least in [0, 1]
 
-def filter_lines(lines, search_method):
-    # remove the 0b in the beginning of bit_criteria
     for i in range(n_char_per_line):
-        left_lines = []
-        check_char = search_method(lines, i)
-        for line in lines:
-            if line[i] == check_char:
-                left_lines.append(line)
+        check_char = count_most_least_char(lines, i)[most_or_least]
+        left_lines = [line for line in lines if line[i] == check_char]
         if len(left_lines) == 1:
             return left_lines[0]
         if len(left_lines) == 0:
             print("nothing left. something wrong. break here")
             1/0
         lines = left_lines
-        left_lines = []
 
     print("nothing left. something wrong. break here")
     1/0
 
-b_oxygen_gen_rating = "0b{}".format(filter_lines(lines_input, count_most_common))
-b_co2_scrubber_rating = "0b{}".format(filter_lines(lines_input, count_least_common))
+
+b_oxygen_gen_rating = "0b{}".format("".join(filter_lines(lines, 0)))
+b_co2_scrubber_rating = "0b{}".format("".join(filter_lines(lines, 1)))
 
 print("oxgen: {}, co2: {}".format(b_oxygen_gen_rating, b_co2_scrubber_rating))
 
