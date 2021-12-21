fbbe8ea2ab5fbfe672b95b069dca827ce273f11c
wenliang <lu@de42.net>
2021-12-21 12:17:29 -0800

    minor refactor day04

--- 2021/day04.py
+++ 2021/day04.py
@@ -19,7 +19,7 @@ n_row = 5
 n_col = 5
 # same size as blocks
 
-def mark(num, blocks):
+def mark(blocks, num):
     def equal_to_zero(a, num):
         return a if a != num else 0
     return [[[equal_to_zero(a, num) for a in b ] for b in c] for c in blocks]
@@ -29,18 +29,18 @@ def check_block(block):
     transposed = list(zip(*block))
     return any([sum(a) == 0 for a in block]) or any([sum(a)==0 for a in transposed])
 
-def find_first_bingo(blocks):
+def find_first_bingo(blocks, num_seq):
     for num in num_seq:
         # print(num)
-        blocks = mark(num, blocks)
-        for i_block, block in enumerate(blocks):
-            if check_block(blocks[i_block]):
-                return i_block, num
+        blocks = mark(blocks, num)
+        blocks_matched = [block for block in blocks if check_block(block)]
+        if len(blocks_matched) > 0:
+            return blocks_matched[0], num
 
-def find_last_bingo(blocks):
+def find_last_bingo(blocks, num_seq):
     unmatched_blocks = list(range(n_block))
     for num in num_seq:
-        blocks = mark(num, blocks)
+        blocks = mark(blocks, num)
         blocks_left = [b for b in blocks if not check_block(b)]
         if len(blocks) == 1 and len(blocks_left) == 0:
             return blocks[0], num
@@ -48,15 +48,16 @@ def find_last_bingo(blocks):
             blocks = blocks_left
 
 def count_unmatched(block):
+    # flatten the 2D block into 1D. then add it up
     return sum(list(chain.from_iterable(block)))
 
 # Q1
-# block, num = find_first_bingo(blocks)
-# sumed = count_unmatched(block)
-# print(num, i_block, num*sumed)
+block, num = find_first_bingo(blocks, num_seq)
+sumed = count_unmatched(block)
+print("Q1: last num {}\nfirst block: \n{}\nanswer: {}".format(num, block, num*sumed))
 
 # Q2
-block, num = find_last_bingo(blocks)
+block, num = find_last_bingo(blocks, num_seq)
 sumed = count_unmatched(block)
-print(num, num*sumed)
+print("Q2: last num {}\nlast block: \n{}\nanswer: {}".format(num, block, num*sumed))
 
