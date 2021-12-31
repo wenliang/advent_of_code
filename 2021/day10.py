0a1bc55832a4dd86515e73457f2b824bf3c9eee1
wenliang <lu@de42.net>
2021-12-30 23:10:24 -0800

    day10/q2

--- 2021/day10.py
+++ 2021/day10.py
@@ -19,6 +19,8 @@ pairs = {
         "<": ">",
         }
 
+########################################
+# q1
 scores = {
         ")": 3,
         "]": 57,
@@ -40,4 +42,33 @@ def find_1st_bad(l):
 bad_1st = [find_1st_bad(l) for l in lines]
 print(bad_1st)
 print(sum(bad_1st))
+
+########################################
+# q2
+scores_q2 = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+        }
+
+def find_uncompleted(l):
+    stack = []
+    for c in l:
+        if c in part1:
+            stack.append(c)
+        elif c in part2:
+            exp_c = pairs[stack.pop()]
+            if c != exp_c:
+                return None
+
+    # stack should be all part1
+    rights = [scores_q2[pairs[a]]*5**i for i,a in enumerate(stack)]
+    return sum(rights)
+
+scores = [find_uncompleted(l) for l in lines]
+scores_valid = [s for s in scores if s]
+scores = sorted(scores_valid)
+print(scores[int(len(scores)/2)])
+embed()
 
