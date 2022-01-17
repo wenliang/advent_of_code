b5447e8717d34f345dff3a60cb33685e9ec78d95
wenliang <lu@de42.net>
2022-01-16 17:13:15 -0800

    update readme

--- 2021/day17.py
+++ 2021/day17.py
@@ -5,7 +5,9 @@ max_x = 286
 min_y = -101
 max_y = -57
 
+# the range to search for velocity
 vy_max = 10000
+# for x-direction, it will overshoot if vx_max > max_x+1
 vx_max = max_x+1
 step_max = 10000
 
@@ -14,6 +16,8 @@ def check_valid_vy(vy, step_max=step_max, min_y=min_y, max_y=max_y):
     distances = list(accumulate(vys))
     steps_valid = [i+1 for i, a in enumerate(distances) if a>=min_y and a <=max_y]
     if len(steps_valid) > 0:
+        # NOTE: steps_valid is important for q2. 
+        # only when one y and one x have common step, this pair will valid
         return vy, max(distances), steps_valid
     else:
         return 0
@@ -35,12 +39,14 @@ vys = [(a[0], a[2]) for a in all_vys if a != 0]
 all_vxs = [check_valid_vx(vx) for vx in range(vx_max)]
 vxs = [a for a in all_vxs if a != 0]
 
+print("possible y and corresponding of step number:")
 print([(a[0], len(a[1])) for a in vys])
+print("\npossible x and corresponding of step number:")
 print([(a[0], len(a[1])) for a in vxs])
 
 # q1
 heights = [a[1] for a in all_vys if a!=0]
-print(max(heights))
+print("\nq1: {}".format(max(heights)))
 
 # q2 
 valid_start = []
@@ -52,5 +58,5 @@ for vy, steps_y in vys:
             valid_start.append((vx, vy))
 
 # print(valid_start)
-print(len(valid_start))
+print("\nq2: {}".format(len(valid_start)))
 
