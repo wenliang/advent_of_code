cccc491eaeb474aa17b7fa5752d2b35df5be9b58
wenliang <lu@de42.net>
2022-01-16 17:08:11 -0800

    day17/q2. use set to find common steps

--- 2021/day17.py
+++ 2021/day17.py
@@ -6,16 +6,51 @@ min_y = -101
 max_y = -57
 
 vy_max = 10000
+vx_max = max_x+1
 step_max = 10000
 
 def check_valid_vy(vy, step_max=step_max, min_y=min_y, max_y=max_y):
     vys = list(range(vy, vy-step_max, -1))
     distances = list(accumulate(vys))
-    distances_valid = [a for a in distances if a>=min_y and a <=max_y]
-    if len(distances_valid) > 0:
-        return max(distances)
+    steps_valid = [i+1 for i, a in enumerate(distances) if a>=min_y and a <=max_y]
+    if len(steps_valid) > 0:
+        return vy, max(distances), steps_valid
     else:
         return 0
 
-heights = [check_valid_vy(vy) for vy in range(vy_max)]
+def check_valid_vx(vx, step_max=step_max, min_x=min_x, max_x=max_x):
+    vxs = list(range(vx, vx-step_max, -1))
+    vxs = [a if a>=0 else 0 for a in vxs]
+    distances = list(accumulate(vxs))
+    steps_valid = [i+1 for i, a in enumerate(distances) if a>=min_x and a <=max_x]
+    if len(steps_valid) > 0:
+        return vx, steps_valid
+    else:
+        return 0
+
+# NOTE: the possible velocity of y could be negative in q2
+all_vys = [check_valid_vy(vy) for vy in range(min_y, vy_max)]
+vys = [(a[0], a[2]) for a in all_vys if a != 0]
+
+all_vxs = [check_valid_vx(vx) for vx in range(vx_max)]
+vxs = [a for a in all_vxs if a != 0]
+
+print([(a[0], len(a[1])) for a in vys])
+print([(a[0], len(a[1])) for a in vxs])
+
+# q1
+heights = [a[1] for a in all_vys if a!=0]
 print(max(heights))
+
+# q2 
+valid_start = []
+for vy, steps_y in vys:
+    for vx, steps_x in vxs:
+        set_y = set(steps_y)
+        set_x = set(steps_x)
+        if len(set_y.intersection(set_x)) > 0:
+            valid_start.append((vx, vy))
+
+# print(valid_start)
+print(len(valid_start))
+
